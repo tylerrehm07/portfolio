fd447df46a160b9becca40f3f9ca6b62da3930dc
Tyler Rehm <tyler@hackrforce.com>
2017-02-21 19:50:50 -0500

    add bootstrap; refactor splash page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,82 +1,28 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>Laravel</title>
+@section('styles')
+    <link href="/css/welcome.css" rel="stylesheet" type="text/css">
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+    <div class="flex-center position-ref full-height">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <div class="content">
+            <div class="title m-b-md">
+                Tyler Rehm
+            </div>
 
-            .full-height {
-                height: 100vh;
-            }
+            <div class="links">
+                <a href="/katas/fizz_buzz">Fizz Buzz</a>
+                <a href="/katas/info">Php Info</a>
+                <a href="/katas/debug">Debug</a>
+            </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Tyler Rehm
-                </div>
-
-                <div class="links">
-                    <a href="/katas/fizz_buzz">Fizz Buzz</a>
-                    <a href="/katas/info">Php Info</a>
-                    <a href="/katas/debug">Debug</a>
-                </div>
-
-                <div class="links">
-                    <a href="https://github.com/tylerrehm07/portfolio">Github</a>
-                    <a href="https://www.linkedin.com/in/tylerrehm/">LinkedIn</a>
-                </div>
+            <div class="links">
+                <a href="https://github.com/tylerrehm07/portfolio">Github</a>
+                <a href="https://www.linkedin.com/in/tylerrehm/">LinkedIn</a>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
+
